2b3d811c153b54d716dec7b72e97abac74793f88
duongktrn <duongktrn@gmail.com>
2022-12-24 13:01:07 +0700

    3 commit

--- index.py
+++ index.py
@@ -1,2 +1,12 @@
 import pandas as pd
 import openpyxl as op 
+
+# cách để git lên github
+#1. tải git về máy tính ,sau đó gõ lệnh git init
+#2 .git remote add origin https://github.com/duongktrn/excel-python.git
+#3.   git config --global user.email "you@example.com" 
+#4. git config --global user.name "Your Name"
+# git add . (để add tất cả file)
+# git commit - m "tên file" để commit file
+# git push origin master 
+
